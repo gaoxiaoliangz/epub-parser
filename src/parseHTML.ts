c70d7355ffcd4186856ab0eac2938a5333ccd637
gaoxiaoliangz <gaoxiaoliangz@gmail.com>
2019-08-22 00:17:21 +0800

    rename stuff

--- src/parseHTML.ts
+++ src/parseHTML.ts
@@ -1,16 +1,10 @@
 import { JSDOM } from 'jsdom'
 import _ from 'lodash'
-import { parseNestedObject } from './utils'
-
-const debug = require('debug')('readr:html')
+import { traverseNestedObject } from './utils'
 
 const OMITTED_TAGS = ['head', 'input', 'textarea', 'script', 'style', 'svg']
 const UNWRAP_TAGS = ['body', 'html', 'div', 'span']
 const PICKED_ATTRS = ['href', 'src', 'id']
-
-const parseRawHTML = HTMLString => {
-  return new JSDOM(HTMLString).window.document.documentElement
-}
 
 /**
  * recursivelyReadParent
@@ -37,22 +31,21 @@ const recursivelyReadParent = (node, callback, final?) => {
   return _read(node)
 }
 
-export interface ParseHTMLObjectConfig {
+export interface ParseHTMLConfig {
   resolveSrc?: (src: string) => string
   resolveHref?: (href: string) => string
 }
-const parseHTMLObject = (HTMLString, config: ParseHTMLObjectConfig = {}) => {
-  debug('parseHTMLObject')
-  const rootNode = parseRawHTML(HTMLString)
+const parseHTML = (HTMLString, config: ParseHTMLConfig = {}) => {
+  const rootNode = new JSDOM(HTMLString).window.document.documentElement
   const { resolveHref, resolveSrc } = config
 
   // initial parse
-  return parseNestedObject(rootNode, {
+  return traverseNestedObject(rootNode, {
     childrenKey: 'childNodes',
     preFilter(node) {
       return node.nodeType === 1 || node.nodeType === 3
     },
-    parser(node, children) {
+    transformer(node, children) {
       if (node.nodeType === 1) {
         const tag = node.tagName.toLowerCase()
         const attrs: GeneralObject = {}
@@ -116,4 +109,4 @@ const parseHTMLObject = (HTMLString, config: ParseHTMLObjectConfig = {}) => {
   }) as HtmlNodeObject[]
 }
 
-export default parseHTMLObject
+export default parseHTML
