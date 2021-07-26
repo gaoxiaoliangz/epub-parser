b089c972d7c29a2af326395d4fe8813455eb63f2
gaoliang.z <gaoliang.z@bytedance.com>
2021-07-26 11:28:53 +0800

    chore: add toc tests

--- src/parseEpub.spec.ts
+++ src/parseEpub.spec.ts
@@ -3,23 +3,27 @@ import _ from 'lodash'
 import * as path from 'path'
 
 const baseDir = process.cwd()
-const filesToBeTested = ['file-1', 'file-2', 'file-3', 'file-4', 'file-1-no-toc']
+const filesToBeTested = ['file-1', 'file-2', 'file-3', 'file-4', 'file-1-no-toc', 'wells']
 
 const testFile = (filename: string) => {
   describe(`parser 测试 ${filename}.epub`, () => {
     const fileContent = parser(path.join(baseDir, `fixtures/${filename}.epub`), {
       type: 'path',
       expand: true,
-    }).catch((error) => {
-      console.log(error)
     })
 
-    test('Result should have keys', (done) => {
-      fileContent.then((result) => {
-        const keys = _.keys(result)
-        expect(keys.length).not.toBe(0)
-        done()
-      })
+    test('Result should have keys', async () => {
+      const keys = _.keys(await fileContent)
+      expect(keys.length).not.toBe(0)
+    })
+
+    test('toc', async () => {
+      const result = await fileContent
+      if (filename === 'file-1-no-toc') {
+        expect(result.structure).toBe(undefined)
+      } else {
+        expect(fileContent && typeof fileContent).toBe('object')
+      }
     })
 
     // it('key 分别为: flesh, nav, meta', done => {
